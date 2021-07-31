48136f498f1c7e70210e2e3bcd83faea76e5aac5
oscar Vaquera <vaqueraoscar0@gmail.com>
2021-07-30 18:20:40 -0700

    Added new source for database

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { Robots } from './Robots';
 import './App.css';
 
 class App extends Component{
     constructor() {
         super()
         this.state = {
-            Robots: Robots,
+            Robots: [],
             searchField: ''
         }
+
+    }
+
+    //Extracts Data from source
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ Robots: users}));
     }
 
     onSearchChange = (event) =>{
@@ -18,17 +25,22 @@ class App extends Component{
     }
 
     render() {
-        const filteredRobots = this.state.Robots.filter(Robots =>{
+        const filteredRobots = this.state.Robots.filter(Robots => {
             return Robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         })
 
-        return (
-            <div className={'tc'}>
-                <h1 className={'f2'}>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList Robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.Robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+
+            return (
+                <div className={'tc'}>
+                    <h1 className={'f2'}>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList Robots={filteredRobots}/>
+                </div>
+            );
+        }
     }
 }
 
